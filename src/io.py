6591694e5eab7d2b745aeef5c575d1da286054a9
Kelvin-T-Lu <61221557+Kelvin-T-Lu@users.noreply.github.com>
2023-03-27 08:11:51 -0400

    uniprot_fasta_to_df implemented

--- src/io.py
+++ src/io.py
@@ -1,26 +1,48 @@
 
 from Bio.SeqIO.FastaIO import FastaIterator
 from pathlib import Path
-import pandas 
+import pandas as pd
 
 INPUT_DIR = Path(r"data/uniprot_sprot.fasta")
 
-def parse_fasta(input_dir:Path):
+def uniprot_fasta_to_df(input_dir:Path) -> pd.DataFrame:
+    """
+        Parse the uniprot fasta data and put it into a Dataframe. 
+    """
+    # output = pd.DataFrame()
+    db = []
+    unique_id = []
+    entry_name = []
+    sequence = []
 
     with open(input_dir) as handle:
         for index,value in enumerate(FastaIterator(handle)):
-            print(value)
-            if index == 5: 
-                break
+            ids = value.name.split("|")
+
+            db.append(ids[0])
+            unique_id.append(ids[1])
+            entry_name.append(ids[2])
+            sequence.append(value.seq)
             
+            # if index == 10:
+                # break
+
+    output = pd.DataFrame({
+        "db":db, 
+        "unique_id": unique_id, 
+        "entry_name": entry_name,
+        "sequence": sequence
+    })
+
+    # print(output)
+    return output
 
 
 
 def main(): 
     """ Test function """
 
-    print("Foo bar")
-    parse_fasta(INPUT_DIR)
+    uniprot_fasta_to_df(INPUT_DIR)
 
 if __name__ == "__main__":
     main()
