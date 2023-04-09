393786dd36c5929ca80d1282d0bdc0a90ac056b5
Kelvin-T-Lu <61221557+Kelvin-T-Lu@users.noreply.github.com>
2023-04-08 20:31:22 -0400

    generate_balanced_dataset

--- src/cs747/uniprot.py
+++ src/cs747/uniprot.py
@@ -218,16 +218,24 @@ def create_test_data(data_dir='test/cs747/data') -> None:
     print(f"Test data created in {data_dir}")
 
 
-def build_percentage_label_stats(data_dir:Path = FASTA_FILE_PATH, label:str = "label") -> dict:
+def generate_fake_lbl(df, header): 
+    """ Delete this function """
+    # TODO - Delete function 
+    fake_labels = [f"label{x}" for x in range(8)]
+    df[header] = df["db"].apply(lambda x: random.choice(fake_labels))
+
+    return df
+
+
+def build_percentage_label_stats(data_dir:Path = FASTA_FILE_PATH, header:str = "label") -> dict:
 
     df = parse_fasta_df(data_dir)
 
-    # TODO - Fake labels (delete following block)
-    fake_labels = [f"label{x}" for x in range(8)]
-    df[label] = df["db"].apply(lambda x: random.choice(fake_labels))
+    # TODO - Fake labels (DELETE)
+    df = generate_fake_lbl(df, header)
     
     # Count all of the label and divide it by population. 
-    percentage_df = df[label].value_counts().apply(lambda x: x/len(df))
+    percentage_df = df[header].value_counts().apply(lambda x: x/len(df))
 
     # Convert df into dict. 
     percentage_dict = percentage_df.to_dict()
@@ -236,4 +244,20 @@ def build_percentage_label_stats(data_dir:Path = FASTA_FILE_PATH, label:str = "l
 
     return percentage_dict
 
+def generate_balanced_data(data_dir:Path = FASTA_FILE_PATH, header:str = "label", frac_population:float = 0.03) -> pd.DataFrame:
+    """ Genereate a balanced dataset based on the fraction of the population. """
+    df = parse_fasta_df(data_dir)
 
+    # TODO - Fake labels - DELETE
+    df = generate_fake_lbl(df, header)
+
+    population_number = round(frac_population * len(df))
+
+    output_df = df.groupby(header).sample(population_number)
+
+    return output_df
+
+
+if __name__ == "__main__":
+
+    generate_balanced_data()
